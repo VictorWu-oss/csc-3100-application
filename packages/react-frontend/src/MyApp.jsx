6077b3b244fc78a933685c99629747c287507ebf
VictorWu-oss <victorwu1208@gmail.com>
2026-09-02 10:05:04 -0700

    Finished Lab 2 Implementation

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,16 @@
 // src/MyApp.jsx
 import React from "react";
 import Table from "./Table";
-import {useState} from "react"
+import {useState} from "react";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-    name: "Charlie",
-    job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    }, // rest of the data
-  ]);
+
+  // characters is the current state value
+  // setCharacters is the function that lets you update it
+  // useState call returns the aforementioned pairs
+  // remove hard-coded characters list and now appending to an empty list
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index){
     const updated = characters.filter((character, i) => {
@@ -30,9 +19,14 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person){
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter}/>
+      <Form handleSubmit={updateList}  />
     </div>
   );
 }
